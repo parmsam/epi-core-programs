486db72bcde45e14078e8a2659c4f39a9f2e37ca
psamrai <40672605+psamrai@users.noreply.github.com>
2018-06-29 22:42:28 -0400

    Update checkload.r

--- checkload.r
+++ checkload.r
@@ -1,7 +1,8 @@
 #mini-program called checkload 
-#to install package if the package cannot be loaded
-#my current way of checking to see if need install and then loading necessary packages
-#may improve :)
+#purpose: to install package if the package cannot be loaded
+#my current way of checking to see if need install then installing and loading necessary packages  
+#creation date: 06/29/2018
+#update: 
 checkload<-function(packages){
   for (i in packages){
     if (!require(i, character.only = TRUE)){
