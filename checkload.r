cba7e1d63a4663104ba43a371dbf35a3e960418c
parmsam <40672605+parmsam@users.noreply.github.com>
2019-02-10 22:07:57 -0500

    Update checkload.r

--- checkload.r
+++ checkload.r
@@ -3,12 +3,13 @@
 #purpose: create a function to install package if the package cannot be loaded and load all relevant packages
 #significance: my current way of checking to see if need package install, installing new ones, and loading all necessary packages  
 #creation date: 06/29/2018
-#update: 
-checkload<-function(packages){
-  for (i in packages){
-    if (!require(i, character.only = TRUE)){
-      cat(i, "=not installed, so installing and loading")
-      install.packages(i)
-      library(i)} 
-  }
-}
+
+#ignore below
+#checkload<-function(packages){
+#  for (i in packages){
+#    if (!require(i, character.only = TRUE)){
+#      cat(i, "=not installed, so installing and loading")
+#      install.packages(i)
+#      library(i)} 
+#  }
+#}
